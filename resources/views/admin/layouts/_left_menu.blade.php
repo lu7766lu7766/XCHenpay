112e735f8d7d4ee92ef8b03dcae30764047fdd53
vitochen <vitochen15@gmail.com>
2018-04-05 20:11:28 +0800

    註解 task功能

--- resources/views/admin/layouts/_left_menu.blade.php
+++ resources/views/admin/layouts/_left_menu.blade.php
@@ -62,14 +62,14 @@
         </ul>
     </li>
 
-    <li {!! (Request::is('admin/tasks') ? 'class="active"' : '') !!}>
-        <a href="{{ URL::to('admin/tasks') }}">
-            <i class="livicon" data-c="#EF6F6C" data-hc="#EF6F6C" data-name="list-ul" data-size="18"
-               data-loop="true"></i>
-            Tasks
-            <span class="badge badge-danger" id="taskcount">{{ Request::get('tasks_count') }}</span>
-        </a>
-    </li>
+    {{--<li {!! (Request::is('admin/tasks') ? 'class="active"' : '') !!}>--}}
+        {{--<a href="{{ URL::to('admin/tasks') }}">--}}
+            {{--<i class="livicon" data-c="#EF6F6C" data-hc="#EF6F6C" data-name="list-ul" data-size="18"--}}
+               {{--data-loop="true"></i>--}}
+            {{--Tasks--}}
+            {{--<span class="badge badge-danger" id="taskcount">{{ Request::get('tasks_count') }}</span>--}}
+        {{--</a>--}}
+    {{--</li>--}}
 
     <li {!! (Request::is('admin/activity_log') ? 'class="active"' : '') !!}>
         <a href="{{  URL::to('admin/activity_log') }}">
