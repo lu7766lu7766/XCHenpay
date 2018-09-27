921669a598be4b3084a8a50b16df5588a8d0fcd9
dayaponyuyen <dayaponyuyen@gmail.com>
2018-09-27 19:33:00 +0800

    下發管理 - 增加小計及總計顯示

--- app/Http/Controllers/Admin/LendManageController.php
+++ app/Http/Controllers/Admin/LendManageController.php
@@ -3,14 +3,14 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use App\Repositories\LendRecords;
-use Yajra\DataTables\DataTables;
-use Illuminate\Http\Request;
 use App\LendRecord;
-use Validator;
+use App\Repositories\LendRecords;
 use App\User;
-use Sentinel;
+use Illuminate\Http\Request;
 use Response;
+use Sentinel;
+use Validator;
+use Yajra\DataTables\DataTables;
 
 class LendManageController extends Controller
 {
@@ -27,40 +27,53 @@ class LendManageController extends Controller
     public function data(LendRecords $lendRecords)
     {
         $records = collect();
-
-        if(!isset(request()->companyId)) {
+        if (!isset(request()->companyId)) {
             return $this->makeDataTable($records);
         }
-
-        if(request()->companyId == $this::ALLCOMPANIES) {
+        if (request()->companyId == $this::ALLCOMPANIES) {
             $records = $lendRecords->getAllRecords(request()->startDate, request()->endDate);
-        }
-        else {
+        } else {
             $records = $lendRecords->getUserRecords(request()->companyId, request()->startDate, request()->endDate);
         }
 
         return $this->makeDataTable($records);
     }
 
+    /**
+     * 計算總和
+     * @param LendRecords $lendRecords
+     * @return LendRecords|\Illuminate\Database\Eloquent\Model|null
+     */
+    public function total(LendRecords $lendRecords)
+    {
+        $userId = null;
+        if (request()->userId != self::ALLCOMPANIES) {
+            $userId = request()->userId;
+        }
+
+        return $lendRecords->getTotal(request()->startDate, request()->endDate, $userId);
+    }
+
     private function makeDataTable($lendRecords)
     {
         return DataTables::of($lendRecords)
-            ->addColumn('company_name',function($lendRecord){
+            ->addColumn('company_name', function ($lendRecord) {
                 return $lendRecord->user->company_name;
             })
-            ->addColumn('account_name',function($lendRecord){
+            ->addColumn('account_name', function ($lendRecord) {
                 return $lendRecord->account->name;
             })
-            ->addColumn('account',function($lendRecord){
+            ->addColumn('account', function ($lendRecord) {
                 return $lendRecord->account->account;
             })
-            ->addColumn('tatol_amount',function($lendRecord){
+            ->addColumn('tatol_amount', function ($lendRecord) {
                 return $lendRecord->amount - $lendRecord->fee;
             })
-            ->addColumn('actions',function($lendRecord){
-                $lendLink = '<a href='. route('admin.lendManage.manageRecord', ['lendRecord' => $lendRecord->id]) .' data-toggle="modal" data-target="#lend_manage"><i class="livicon" data-name="edit" data-size="18" data-loop="true" data-c="#f56954" data-hc="#f56954" title=' . trans('Trade/LendManage/form.manage') . '></i></a>';
-                $infoLink = '<a href='. route('admin.lendManage.showRecord', ['lendRecord' => $lendRecord->id]) .' data-toggle="modal" data-target="#lend_info"><i class="livicon" data-name="info" data-size="18" data-loop="true" data-c="#428BCA" data-hc="#428BCA" title=' . trans('Trade/LendManage/form.manage') . '></i></a>';
-
+            ->addColumn('actions', function ($lendRecord) {
+                $lendLink = '<a href=' . route('admin.lendManage.manageRecord',
+                        ['lendRecord' => $lendRecord->id]) . ' data-toggle="modal" data-target="#lend_manage"><i class="livicon" data-name="edit" data-size="18" data-loop="true" data-c="#f56954" data-hc="#f56954" title=' . trans('Trade/LendManage/form.manage') . '></i></a>';
+                $infoLink = '<a href=' . route('admin.lendManage.showRecord',
+                        ['lendRecord' => $lendRecord->id]) . ' data-toggle="modal" data-target="#lend_info"><i class="livicon" data-name="info" data-size="18" data-loop="true" data-c="#428BCA" data-hc="#428BCA" title=' . trans('Trade/LendManage/form.manage') . '></i></a>';
                 $action = $infoLink;
                 $action .= $lendLink;
 
@@ -70,7 +83,8 @@ class LendManageController extends Controller
             ->make(true);
     }
 
-    public function showRecordDialog(LendRecord $lendRecord){
+    public function showRecordDialog(LendRecord $lendRecord)
+    {
         $account = $lendRecord->account;
 
         return view('admin.trade.showRecordModal', compact('lendRecord', 'account'));
@@ -87,36 +101,29 @@ class LendManageController extends Controller
             'required' => ':attribute 不得为空.',
             'exists'   => '此不存在，请刷新页面后重试.',
         ];
-
-        $validator = Validator::make( $request->toArray(), [
-            'id' => 'required|exists:lend_records,id',
+        $validator = Validator::make($request->toArray(), [
+            'id'        => 'required|exists:lend_records,id',
             'operation' => 'required|in:0,1',
-        ],$messages);
-
-        if ($validator->fails())
+        ], $messages);
+        if ($validator->fails()) {
             return $this->validateErrorResponseInJson($validator);
-
+        }
         $record = LendRecord::find($request->id);
-
         $user = Sentinel::getUser();
-
-        if($request->operation){
+        if ($request->operation) {
             $record->update(['lend_state' => LendRecords::ACCEPT_STATE, 'lend_summary' => LendRecords::ACCEPT_SUMMARY]);
-
             activity($user->email)
                 ->causedBy($user)
                 ->log('准許一笔下发申请 ' . $record->record_seq);
-        }
-        else{
+        } else {
             $record->update(['lend_state' => LendRecords::DENY_STATE, 'lend_summary' => LendRecords::DENY_SUMMARY]);
-
             activity($user->email)
                 ->causedBy($user)
                 ->log('拒绝一笔下发申请 ' . $record->record_seq);
         }
 
-        return Response::json(array(
+        return Response::json([
             'Result' => 'OK'
-        ));
+        ]);
     }
 }
