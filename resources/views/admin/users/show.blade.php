b2050aa3cd662e96c375b8eb3d2a3cec5fc04ca9
Aaron <peilun.cc@gmail.com>
2018-11-20 21:52:03 +0800

    Aaron#7 (#3)
    * 移除-多餘的檔案
    
    * 新增-商戶資料相關程式

--- resources/views/admin/users/show.blade.php
+++ resources/views/admin/users/show.blade.php
@@ -21,6 +21,91 @@
 
 @section('content')
     {{--放置 content--}}
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="card m-b-30">
+                <div class="card-body">
+                    <form class="form-horizontal" id="commentForm" data-parsley-validate="">
+                        {{ csrf_field() }}
+                        <div class="form-group row">
+                            <label class="col-sm-2 control-label">商戶名稱</label>
+                            <div class="col-sm-10 p-t-7 border-line">{{ $user->company_name }}</div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-sm-2 control-label">company_service_id</label>
+                            <div class="col-sm-10 p-t-7 border-line">{{ $user->company_service_id }}</div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-sm-2 control-label">sceret_key</label>
+                            <div class="col-sm-10 p-t-7 border-line">{{ $user->sceret_key }}</div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-sm-2 control-label">状态</label>
+                            <div class="col-sm-10 p-t-7 border-line">
+                                @if ($user->status == 'on')
+                                    <i class="mdi mdi-check-circle-outline text-green text-lg"></i>
+                                @else
+                                    <i class="mdi mdi-check-close-outline text-red text-lg"></i>
+                                @endif
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-sm-2 control-label">下发申请</label>
+                            <div class="col-sm-10 p-t-7 border-line">
+                                @if ($user->apply_status == 'on')
+                                    <i class="mdi mdi-check-circle-outline text-green text-lg"></i>
+                                @else
+                                    <i class="mdi mdi-check-close-outline text-red text-lg"></i>
+                                @endif
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-sm-2 control-label">联络电话</label>
+                            <div class="col-sm-10 p-t-7 border-line">{{ $user->mobile }}</div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-sm-2 control-label">电子邮箱</label>
+                            <div class="col-sm-10 p-t-7 border-line">{{ $user->email }}</div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-sm-2 control-label">QQ号</label>
+                            <div class="col-sm-10 p-t-7 border-line">{{ $user->QQ_id }}</div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-sm-2 control-label required">旧密码 <b>*</b></label>
+                            <div class="col-sm-10">
+                                <input type="password" class="form-control" placeholder="请输入旧密码" id="oldPassword"
+                                       name="oldPassword" required>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-sm-2 control-label required">新密码 <b>*</b></label>
+                            <div class="col-sm-10">
+                                <input type="password" class="form-control" placeholder="请输入密码" id="password"
+                                       name="password" required>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-sm-2 control-label required">密码确认 <b>*</b></label>
+                            <div class="col-sm-10">
+                                <input type="password" class="form-control" placeholder="请再一次输入新密码"
+                                       id="password_confirm" name="password_confirm" required>
+                            </div>
+                        </div>
+                        <div class="form-group row justify-content-end">
+                            <div class="col-sm-10">
+                                <button type="button" class="btn btn-primary waves-effect waves-light"
+                                        id="change-password">提交
+                                </button>
+                                <button type="reset" class="btn btn-secondary waves-effect">重设</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <!-- card end -->
+        </div>
+    </div>
 @stop
 
 @section('modal')
@@ -29,4 +114,80 @@
 
 @section('footer_scripts')
     {{--放置 js--}}
+    <script src="{{ asset('plugins/bootstrap-validator/js/bootstrapValidator.min.js') }}"></script>
+    {{--<script src="{{ asset('plugins/bootstrap-validator/i18n/zh_cn.js') }}"></script>--}}
+
+    <script>
+        $(function () {
+            $('#change-password').click(function (e) {
+                e.preventDefault();
+
+                var $validator = $('#commentForm').data('bootstrapValidator').validate();
+
+                console.log($('#password').val());
+
+                if ($validator.isValid()) {
+                    var sendData = '_token=' + $("input[name='_token']").val() + '&oldPassword=' + $('#oldPassword').val() + '&password=' + $('#password').val() + '&id=' + {{ $user->id }};
+                    var path = "passwordreset";
+                    $.ajax({
+                        url: path,
+                        type: "post",
+                        data: sendData,
+                        headers: {
+                            'X-CSRF-TOKEN': $('meta[name="csrf_token"]').attr('content')
+                        },
+                        success: function (data) {
+                            $('#oldPassword, #password, #password_confirm').val('');
+                            $validator.resetForm();
+
+                            if (data.Result == 'error') {
+                                alert(data.Message);
+                                return;
+                            }
+                            alert('密码已成功设定');
+                        },
+                        error: function (xhr, ajaxOptions, thrownError) {
+                            alert('设定失败，与服务器供沟通失败');
+                        }
+                    });
+                }
+            });
+        });
+
+        $("#commentForm").bootstrapValidator({
+            fields: {
+                oldPassword: {
+                    validators: {
+                        notEmpty: {
+                            message: '旧密码是必须的'
+                        },
+                    }
+                },
+                password: {
+                    validators: {
+                        notEmpty: {
+                            message: '新密码是必须的'
+                        },
+                        stringLength: {
+                            min: 6,
+                            message: '新密码必多于6个字元'
+                        }
+                    }
+                },
+                password_confirm: {
+                    validators: {
+                        notEmpty: {
+                            message: '密码确认是必须的'
+                        },
+                        identical: {
+                            field: 'password',
+                            message: '密码确认与新密码不符'
+                        }
+                    }
+                }
+            }
+        });
+
+    </script>
+
 @stop
