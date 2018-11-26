cf151e77037b52ca5513f7f08379bf32b7821dff
House <richhuang076@gmail.com>
2018-11-26 15:34:19 +0800

    CountrySeeder 不存在,故移除CountrySeeder call

--- database/seeds/AdminSeeder.php
+++ database/seeds/AdminSeeder.php
@@ -1,77 +1,65 @@
 <?php
 
-use Faker\Factory;
-use App\User;
-use Illuminate\Database\Seeder;
-
-class AdminSeeder extends DatabaseSeeder {
-
-	public function run()
-	{
-		DB::table('users')->truncate(); // Using truncate function so all info will be cleared when re-seeding.
-		DB::table('roles')->truncate();
-		DB::table('role_users')->truncate();
-		DB::table('activations')->truncate();
+class AdminSeeder extends DatabaseSeeder
+{
+    public function run()
+    {
+        DB::table('users')->truncate(); // Using truncate function so all info will be cleared when re-seeding.
+        DB::table('roles')->truncate();
+        DB::table('role_users')->truncate();
+        DB::table('activations')->truncate();
         DB::table('payment_fees')->truncate();
-
-		$admin = Sentinel::registerAndActivate(array(
-			'email'       => 'admin@admin.com',
-			'password'    => "admin",
-		));
-
-        $ser = Sentinel::registerAndActivate(array(
-            'email'       => 'ser@ser.com',
-            'password'    => "ser",
-        ));
-
-        $fan = Sentinel::registerAndActivate(array(
-            'email'       => 'fan@fan.com',
-            'password'    => "fan",
-        ));
-
-        $testUser = Sentinel::registerAndActivate(array(
-            'email'       => 'UUU@uuu.com',
-            'password'    => "unmdzsen",
+        $admin = Sentinel::registerAndActivate([
+            'email'    => 'admin@admin.com',
+            'password' => "admin",
+        ]);
+        $ser = Sentinel::registerAndActivate([
+            'email'    => 'ser@ser.com',
+            'password' => "ser",
+        ]);
+        $fan = Sentinel::registerAndActivate([
+            'email'    => 'fan@fan.com',
+            'password' => "fan",
+        ]);
+        $testUser = Sentinel::registerAndActivate([
+            'email'              => 'UUU@uuu.com',
+            'password'           => "unmdzsen",
             'company_service_id' => "cdaecb46830eb3cb920d8b9856e6438e",
-            'sceret_key'  => "97bc26ce95637a9114fce9d72bde884f",
-            'company_name'=> "99_API_test",
-            'lend_fee'    => 0.0002,
-        ));
-
+            'sceret_key'         => "97bc26ce95637a9114fce9d72bde884f",
+            'company_name'       => "99_API_test",
+            'lend_fee'           => 0.0002,
+        ]);
         $adminRole = Sentinel::getRoleRepository()->createModel()->create([
-			'name' => '管理者',
-			'slug' => 'admin',
-			'permissions' => array(
-                'logQuery' => true,
-                'users' => true,
-                'lendApply' => true,
-                'lendManage' => true,
+            'name'        => '管理者',
+            'slug'        => 'admin',
+            'permissions' => [
+                'logQuery'     => true,
+                'users'        => true,
+                'lendApply'    => true,
+                'lendManage'   => true,
                 'activity_log' => true,
-                'account' => true,
-
+                'account'      => true,
                 'users.dataSwitch' => true,
-            ),
-		]);
-
+            ],
+        ]);
         $ServiceRole = Sentinel::getRoleRepository()->createModel()->create([
-            'name'  => '客服',
-            'slug'  => 'customer_service',
-            'permissions' => array(
-                'users.index' => false,
-                'users.data' => false,
-                'users.show' => false,
-                'users.edit' => false,
-                'users.update' => false,
-                'users.updateProfile' => false,
-                'users.create' => false,
-                'users.store' => false,
-                'users.getModalDelete' => false,
-                'users.destroy' => false,
+            'name'        => '客服',
+            'slug'        => 'customer_service',
+            'permissions' => [
+                'users.index'           => false,
+                'users.data'            => false,
+                'users.show'            => false,
+                'users.edit'            => false,
+                'users.update'          => false,
+                'users.updateProfile'   => false,
+                'users.create'          => false,
+                'users.store'           => false,
+                'users.getModalDelete'  => false,
+                'users.destroy'         => false,
                 'users.getDeletedUsers' => false,
-                'users.getRestore' => false,
-                'users.passwordreset' => false,
-                'users.dataSwitch' => true,
-
+                'users.getRestore'      => false,
+                'users.passwordreset'   => false,
+                'users.dataSwitch'      => true,
                 'account.index'            => true,
                 'account.accountData'      => true,
                 'account.getAccountDelete' => true,
@@ -79,32 +67,29 @@ class AdminSeeder extends DatabaseSeeder {
                 'account.createAccount'    => false,
                 'account.sendVerifyCode'   => false,
                 'account.addAccount'       => false,
-
                 'showLending' => false,
-                'lendApply' => true,
-                'lendManage' => false,
-            ),
+                'lendApply'   => true,
+                'lendManage'  => false,
+            ],
         ]);
-
         $fanRole = Sentinel::getRoleRepository()->createModel()->create([
-            'name'  => '财务',
-            'slug'  => 'finance',
-            'permissions' => array(
-                'users.index' => false,
-                'users.data' => false,
-                'users.show' => false,
-                'users.edit' => false,
-                'users.update' => false,
-                'users.updateProfile' => false,
-                'users.create' => false,
-                'users.store' => false,
-                'users.getModalDelete' => false,
-                'users.destroy' => false,
+            'name'        => '财务',
+            'slug'        => 'finance',
+            'permissions' => [
+                'users.index'           => false,
+                'users.data'            => false,
+                'users.show'            => false,
+                'users.edit'            => false,
+                'users.update'          => false,
+                'users.updateProfile'   => false,
+                'users.create'          => false,
+                'users.store'           => false,
+                'users.getModalDelete'  => false,
+                'users.destroy'         => false,
                 'users.getDeletedUsers' => false,
-                'users.getRestore' => false,
-                'users.passwordreset' => false,
-                'users.dataSwitch' => true,
-
+                'users.getRestore'      => false,
+                'users.passwordreset'   => false,
+                'users.dataSwitch'      => true,
                 'account.index'            => true,
                 'account.accountData'      => true,
                 'account.getAccountDelete' => true,
@@ -112,31 +97,28 @@ class AdminSeeder extends DatabaseSeeder {
                 'account.createAccount'    => false,
                 'account.sendVerifyCode'   => false,
                 'account.addAccount'       => false,
-
                 'showLending' => false,
-                'lendApply' => false,
-                'lendManage' => true,
-            ),
+                'lendApply'   => false,
+                'lendManage'  => true,
+            ],
         ]);
-
         $userRole = Sentinel::getRoleRepository()->createModel()->create([
-			'name'  => '商户',
-			'slug'  => 'user',
-            'permissions' => array(
-                'users.index' => false,
-                'users.data' => false,
-                'users.show' => false,
-                'users.edit' => false,
-                'users.update' => false,
-                'users.updateProfile' => false,
-                'users.create' => false,
-                'users.store' => false,
-                'users.getModalDelete' => false,
-                'users.destroy' => false,
+            'name'        => '商户',
+            'slug'        => 'user',
+            'permissions' => [
+                'users.index'           => false,
+                'users.data'            => false,
+                'users.show'            => false,
+                'users.edit'            => false,
+                'users.update'          => false,
+                'users.updateProfile'   => false,
+                'users.create'          => false,
+                'users.store'           => false,
+                'users.getModalDelete'  => false,
+                'users.destroy'         => false,
                 'users.getDeletedUsers' => false,
-                'users.getRestore' => false,
-                'users.passwordreset' => false,
-
+                'users.getRestore'      => false,
+                'users.passwordreset'   => false,
                 'account.index'            => false,
                 'account.accountData'      => true,
                 'account.getAccountDelete' => true,
@@ -144,20 +126,15 @@ class AdminSeeder extends DatabaseSeeder {
                 'account.createAccount'    => true,
                 'account.sendVerifyCode'   => true,
                 'account.addAccount'       => true,
-
                 'showLending' => true,
-                'lendApply' => false,
-                'lendManage' => false,
-            ),
-		]);
-
-
-		$admin->roles()->attach($adminRole);
+                'lendApply'   => false,
+                'lendManage'  => false,
+            ],
+        ]);
+        $admin->roles()->attach($adminRole);
         $ser->roles()->attach($ServiceRole);
         $fan->roles()->attach($fanRole);
         $testUser->roles()->attach($userRole);
-
-		$this->command->info('Admin User created with username admin@admin.com and password admin');
-	}
-
+        $this->command->info('Admin User created with username admin@admin.com and password admin');
+    }
 }
