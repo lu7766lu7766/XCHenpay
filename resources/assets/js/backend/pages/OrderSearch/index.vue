ee298aad07e398aaee953b8d61129f65c407f938
Jac Wang <lu7766lu7766@gmail.com>
2019-01-07 15:14:21 +0800

    移除手續費列表 (#42)

--- resources/assets/js/backend/pages/OrderSearch/index.vue
+++ resources/assets/js/backend/pages/OrderSearch/index.vue
@@ -3,8 +3,6 @@
         <div class="col-lg-12">
             <company-selector v-model="company_id" :options="companies"/>
             <!-- fee start -->
-            <component :is="company_id ? 'fee' : ''" v-if="company_id !== ''"/>
-            <!-- fee end -->
 
             <!-- order start -->
             <component :is="company_id ? 'order' : ''" v-if="company_id !== ''"/>
@@ -14,33 +12,29 @@
 </template>
 
 <script>
+    import ReqMixins from 'mixins/request'
     export default {
         api: "orderSearch",
+        mixins: [ReqMixins],
         components: {
             CompanySelector: require('@/CompanySelector'),
-            Fee: require('./Fee'),
             Order: require('./Order'),
         },
         data: () => ({
             canEditOrder: false,
-            canEditFee: false,
             canShowCompany: false,
             company_id: '',
             companies: [],
         }),
         async mounted() {
-            let loader = this.$loading.show({
-                container: this.$el,
+            this.proccessAjax('dataInit', {}, res => {
+                this.companies = _.map(res.companies)
+                this.canEditOrder = res.can_edit_order
+                this.canShowCompany = res.can_show_company
+                if (!this.canShowCompany) {
+                    this.company_id = res.user.id
+                }
             })
-            var res = await this.$callApi('orderSearch:dataInit')
-            this.companies = _.map(res.companies)
-            this.canEditFee = res.can_edit_fee
-            this.canEditOrder = res.can_edit_order
-            this.canShowCompany = res.can_show_company
-            if (!this.canShowCompany) {
-                this.company_id = res.user.id
-            }
-            loader.hide()
         }
     }
 </script>
