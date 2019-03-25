37bf941fb0f67c281a0b77567b6b979881f1471e
dayaponyuyen0609 <42618397+dayaponyuyen0609@users.noreply.github.com>
2019-03-25 17:54:12 +0800

    Funny#route cashier company (#117)
    * route Cashier Account Company
    
    * add status

--- routes/web.php
+++ routes/web.php
@@ -411,26 +411,6 @@ Route::group(
         });
     }
 );
-#公司帳戶
-Route::group(
-    [
-        'prefix'     => 'admin/cashier/company',
-        'as'         => 'admin.cashier.company.',
-        'namespace'  => 'Manage\Cashier',
-        'middleware' => ['admin', 'has:management,CompanyAccountPolicy'],
-    ],
-    function () {
-        Route::get('/', 'CompanyAccountController@indexView')->name('view');
-        Route::group(['middleware' => ['json_api']], function () {
-            Route::post('/', 'CompanyAccountController@index')->name('index');
-            Route::post('total', 'CompanyAccountController@total')->name('total');
-            Route::get('{id}/info', 'CompanyAccountController@info')->name('info');
-            Route::get('/channel', 'CompanyAccountController@channel')->name('channel');
-            Route::post('create', 'CompanyAccountController@create')->name('create');
-            Route::put('/', 'CompanyAccountController@update')->name('update');
-        });
-    }
-);
 Route::group(
     ['prefix' => 'pay/gateway/', 'namespace' => 'BankCardGateway'],
     function () {
---
+++ routes/web/cashier_account_company.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * 公司帳戶
+ * Created by PhpStorm.
+ * User: funny
+ * Date: 2019/3/25
+ * Time: 下午 12:09
+ */
+Route::group(
+    [
+        'prefix'     => 'admin/cashier/company',
+        'as'         => 'admin.cashier.company.',
+        'namespace'  => 'Manage\Cashier',
+        'middleware' => ['admin', 'has:management,CompanyAccountPolicy'],
+    ],
+    function () {
+        Route::get('/', 'CompanyAccountController@indexView')->name('view');
+        Route::group(['middleware' => ['json_api']], function () {
+            Route::post('/', 'CompanyAccountController@index')->name('index');
+            Route::post('total', 'CompanyAccountController@total')->name('total');
+            Route::get('{id}/info', 'CompanyAccountController@info')->name('info');
+            Route::get('/channel', 'CompanyAccountController@channel')->name('channel');
+            Route::post('create', 'CompanyAccountController@create')->name('create');
+            Route::put('/', 'CompanyAccountController@update')->name('update');
+            Route::get('status', 'CompanyAccountController@status')->name('status');
+        });
+    }
+);
